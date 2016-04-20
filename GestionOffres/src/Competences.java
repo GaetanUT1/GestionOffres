9174afa602ac8e3008486b0eae5eebf1ae65980a
gaetan <gaetan@PC-GAETAN>
2016-04-20 08:36:06 +0200

    Renommage du fichier pour ce que ca fait. On passe Competence -> Competences

---
+++ GestionOffres/src/Competences.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gaetan
+ */
+public class Competences {
+    /* Pour tester le transfert dans GitHub */
+    
+}
